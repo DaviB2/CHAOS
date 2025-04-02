53b590b505a1ac773a399fe5c2f302f3c24a3d46
Davi Becker <davi_s_becker@estudante.sesisenai.org.br>
2025-04-02 19:35:32 -0300

    Explicação
    explicação do código

--- PrograAplica/aula1.js
+++ PrograAplica/aula1.js
@@ -1,11 +1,14 @@
 const prompt = require(`prompt-sync`)();
 
+// Modelo de atributos e funções para criação de um objeto
 class Cliente{
-    nome;
+    nome; // Atributo
     idade;
     cpf;
     agencia;
 }
+
+// Objeto que possui as informações de classe de forma individual
 let cliente = new Cliente();
 
 cliente.nome = prompt("DIgite o nome do cliente: ");
